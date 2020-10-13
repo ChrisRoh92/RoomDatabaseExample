ccf7aa2150c1d52cdd79d22fa0a6dbc00e4a0141
Christoph <Christoph.Rohnert@t-online.de>
2020-10-13 13:24:43 +0200

    Changed Drawable of the FloatingActionButton to ic_add.xml

--- app/src/main/java/com/example/roomdatabaseexample/main/MainFragment.kt
+++ app/src/main/java/com/example/roomdatabaseexample/main/MainFragment.kt
@@ -1,18 +1,17 @@
-package com.example.roomdatabaseexample
+package com.example.roomdatabaseexample.main
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.RecyclerView
-import com.example.roomdatabaseexample.main.VocListAdapter
+import com.example.roomdatabaseexample.R
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
+
 class MainFragment : Fragment() {
 
     private lateinit var rootView:View
