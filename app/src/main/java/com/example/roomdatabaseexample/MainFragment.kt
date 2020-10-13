a8ee86de14292def545fcfc5fd3b127ee139c267
Christoph <Christoph.Rohnert@t-online.de>
2020-10-13 12:26:57 +0200

    Add RecyclerView to fragment_main.xml
    Create item_voc.xml as list element for the vocs

--- app/src/main/java/com/example/roomdatabaseexample/MainFragment.kt
+++ app/src/main/java/com/example/roomdatabaseexample/MainFragment.kt
@@ -11,21 +11,15 @@ import androidx.navigation.fragment.findNavController
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class MainFragment : Fragment() {
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        return inflater.inflate(R.layout.fragment_main, container, false)
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
-    }
 }
